ef32d3fd4a05d003b32b121bdd4cc2deaf5b86ef
Haroon <haroon20@hotmail.co.uk>
2022-04-10 12:26:53 +0100

    increment button working with useState, initial setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [showText, setShowText] = useState(true);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick ={ () => {
+          setCount(count + 1);
+          setShowText(!showText);
+        }}
+        >
+          Click Here
+
+      </button>
+      { showText && <p>This is a text</p>}
+    </div>
+  )
+}
+
+export default App;
+
